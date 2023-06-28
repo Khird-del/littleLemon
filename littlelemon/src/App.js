0e5b0fdb45a49c0b2fbf3d42fa6c3edbe6c03c14
Nabeel7 <nabeel@aiztek.com>
2023-06-28 05:16:26 +0500

    upgrade

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
-import Header from './Components/Header';
-import Nav from './Components/Nav';
-import Main from './Components/Main';
-import Footer from './Components/Footer';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
+    <>
+      <Navbar />
+      <Routing />
+      <Footer />
+    </>
   );
 }
 
--- littlelemon/src/Components/Footer.js
+++ littlelemon/src/Components/Footer.js
@@ -1,10 +1,76 @@
-import React from 'react'
-
-const Footer = () => {
+import { Link } from "react-router-dom";
+export default function Footer() {
   return (
-    <div style={{alignSelf:'flex-end',backgroundColor:'#495E57', position:"absolute",
-    left:0, bottom:0, right:0}}>Footer</div>
-  )
-}
+    <footer>
+      <menu>
+        <li className="img-footer">
+          <img
+            src={require("../assets/footer-logo.png")}
+            alt="Little Lemon logo"
+          ></img>
+        </li>
+        <li className="contact">
+          <h1 className="footer-header">Navigation</h1>
+          <ul className="footer-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <a
+              href={require("../assets/menu.webp")}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Menu
+            </a>
+            <Link to="/reservations">Reservations</Link>
+            <Link to="/order">Order</Link>
+            <Link to="/login">Login</Link>
+          </ul>
+        </li>
 
-export default Footer
+        <li>
+          <h1 className="footer-header">Contact</h1>
+          <ul className="footer-links">
+            <li>2395 Maldove Way,</li>
+            <li>Chicago Illinois</li>
+            <br></br>
+            <li>(629)-243-6827</li>
+            <br></br>
+            <a
+              href="mailto: info@littlelemon.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              info@littlelemon.com
+            </a>
+          </ul>
+        </li>
+        <li>
+          <h1 className="footer-header">Connect</h1>
+          <ul className="footer-links">
+            <a
+              href="https://www.facebook.com/thelittlelemonshop/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://www.instagram.com/littlelemonmoon/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.youtube.com/watch?v=4WT5cMFySHg&ab_channel=InspiringSquad"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Join us!
+            </a>
+          </ul>
+        </li>
+      </menu>
+    </footer>
+  );
+}
